2a3d6bc337618c2f2cb19f1b5811ff4233dd7b6f
Viacheslav Sychov <sychov.96.slava@gmail.com>
2022-02-20 13:30:56 +0200

    First commit

---
+++ account-service/src/main/java/com/javastart/account/entity/Account.java
@@ -0,0 +1,41 @@
+package com.javastart.account.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.OffsetDateTime;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Account {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long accountId;
+
+    private String name;
+
+    private String email;
+
+    private String phone;
+
+    private OffsetDateTime creatingData;
+
+    @ElementCollection
+    private List<Long> bills;
+
+    public Account(String name, String email, String phone,
+                   OffsetDateTime creatingData, List<Long> bills) {
+        this.accountId = accountId;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.creatingData = creatingData;
+        this.bills = bills;
+    }
+}
